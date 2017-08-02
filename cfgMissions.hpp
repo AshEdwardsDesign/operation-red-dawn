52bb9eae1fdaa367805b733859a85e027c66c32e
Ash Edwards <info@ashedwardsdesign.co.uk>
2017-08-03 00:17:24 +0900

    Update cfgMissions.hpp

--- cfgMissions.hpp
+++ cfgMissions.hpp
@@ -16,7 +16,7 @@ class CfgMissions
 			overviewText = "As a BLUFOR soldier, work with your AI commander to capture the island of Malden from the occupying forces of OPFOR. Or, go it alone and carry out your own missions against the backdrop of a large, ever-evolving conflict.";
 			directory = "A3\operation_red_dawn\MPScenarios\Operation_Red_Dawn.Stratis";
 		};
-		class Operation_Red_Dawn_02
+		class Operation_Red_Dawn_03
 		{
 			briefingName = "Operation Red Dawn (Malden)";
 			author = "Billa";
